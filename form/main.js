35cdaa80af9a854aa36036f1d89f4523f615be47
Paul Greenham <paul.greenham@gmail.com>
2019-03-14 22:35:47 +0200

    correct bugs in form excersize

--- form/main.js
+++ form/main.js
@@ -11,35 +11,46 @@ const removeCommas = function(str){
 }
 
 const validate = function(){
+    const previousErrors = document.getElementsByClassName("error-message")
+    while(previousErrors[0]){
+        container.removeChild(previousErrors[0])
+    }
     const name = document.getElementById("name-input").value
     const salary = document.getElementById("salary-input").value
     const birthday = document.getElementById("birthday-input").value
     const phone = document.getElementById("phone-input").value
+    let errorCount = 0
     if(validateName(name) != undefined){
         const nameError = document.createElement("div")
         nameError.setAttribute("class", "error-message")
         nameError.innerHTML = validateName(name)
         container.appendChild(nameError)
+        errorCount++
     }
     if(validateSalary(salary) != undefined){
         const nameError = document.createElement("div")
         nameError.setAttribute("class", "error-message")
         nameError.innerHTML = validateSalary(salary)
         container.appendChild(nameError)
+        errorCount++
     }
     if(validateBirthday(birthday) != undefined){
         const nameError = document.createElement("div")
         nameError.setAttribute("class", "error-message")
         nameError.innerHTML = validateBirthday(birthday)
         container.appendChild(nameError)
+        errorCount++
     }
     if(validatePhone(phone) != undefined){
         const nameError = document.createElement("div")
         nameError.setAttribute("class", "error-message")
         nameError.innerHTML = validatePhone(phone)
         container.appendChild(nameError)
+        errorCount++
     }
-    else document.getElementById("container").setAttribute("style", "display:none")
+    if(errorCount == 0){
+        document.getElementById("container").setAttribute("style", "display:none")
+    }
 }
 
 const validateName = function(name){
